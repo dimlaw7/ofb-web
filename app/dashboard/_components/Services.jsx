c705615f8770760ad4320e476ae72c0c4e0c9971
dimlaw7 <dimlaw7@gmail.com>
2024-12-10 10:58:08 +0100

    Update Services.jsx

--- app/dashboard/_components/Services.jsx
+++ app/dashboard/_components/Services.jsx
@@ -1,22 +1,27 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import { BookDown } from "lucide-react";
+import { BookUp, PackageOpen } from "lucide-react";
+import { useState } from "react";
+import TopupModal from "./modal/TopupModal";
+import Link from "next/link";
 
 const DashboardServices = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <>
-      <section className="hidden border bg-white px-4 py-8 lg:block">
+      <section className="sm-devices hidden border bg-white px-4 py-8 lg:block">
         <h4 className="font-semibold">What will you like to do?</h4>
-        <div className="mt-8 flex flex-col lg:flex-row">
-          <div className="card mb-4 flex min-h-32 w-full flex-col justify-between rounded bg-purple-200 px-4 py-5 lg:mr-8 lg:w-60">
+        <ul className="mt-8 flex flex-col lg:flex-row">
+          <li className="card mb-4 flex min-h-32 w-full flex-col justify-between rounded bg-purple-200 px-4 py-5 lg:mr-8 lg:w-60">
             <div className="text-sm">
               <h4 className="text-sm font-semibold text-[#232834]">
                 Save To Wallet
               </h4>
               <p className="my-4">Deposit your monthly saving.</p>
             </div>
-            <Button>Click here</Button>
-          </div>
-          <div className="card mb-4 flex min-h-32 w-full flex-col justify-between rounded bg-yellow-100 px-4 py-5 lg:mr-8 lg:w-60">
+            <Button onClick={() => setShowModal(true)}>Click here</Button>
+          </li>
+          <li className="card mb-4 flex min-h-32 w-full flex-col justify-between rounded bg-yellow-100 px-4 py-5 lg:mr-8 lg:w-60">
             <div className="text-sm">
               <h4 className="text-sm font-semibold text-[#232834]">
                 Check My Budget
@@ -26,8 +31,8 @@ const DashboardServices = () => {
               </p>
             </div>
             <Button>Click here</Button>
-          </div>
-          <div className="card mb-4 flex min-h-32 w-full flex-col justify-between rounded bg-lime-100 px-4 py-5 lg:mr-8 lg:w-60">
+          </li>
+          <li className="card mb-4 flex min-h-32 w-full flex-col justify-between rounded bg-lime-100 px-4 py-5 lg:mr-8 lg:w-60">
             <div className="text-sm">
               <h4 className="text-sm font-semibold text-[#232834]">
                 Update Package
@@ -35,56 +40,32 @@ const DashboardServices = () => {
               <p className="my-4">Upgrade or Downgrade your savings package</p>
             </div>
             <Button>Click here</Button>
-          </div>
-        </div>
+          </li>
+        </ul>
       </section>
-      <section className="border bg-white px-4 py-6 lg:hidden">
+      <section className="services border bg-white px-4 py-6 lg:hidden">
         <h4 className="text-sm font-semibold">What would you like to do?</h4>
-        <div className="mt-6 grid grid-cols-4 gap-4">
-          <div className="cols flex flex-col items-center">
-            <div className="flex h-10 w-10 items-center justify-center rounded-full bg-blue-100">
-              <BookDown size={14} />
+        <ul className="mt-6 grid grid-cols-4 gap-4">
+          <li
+            onClick={() => setShowModal(true)}
+            className="quick-save cols flex flex-col items-center"
+          >
+            <div className="flex h-10 w-10 items-center justify-center rounded-full bg-purple-200">
+              <BookUp size={15} />
             </div>
-            <span className="text-xs">Title</span>
-          </div>
-          <div className="cols flex flex-col items-center">
-            <div className="flex h-10 w-10 items-center justify-center rounded-full bg-blue-100">
-              <BookDown size={14} />
-            </div>
-            <span className="text-xs">Title</span>
-          </div>
-          <div className="cols flex flex-col items-center">
-            <div className="flex h-10 w-10 items-center justify-center rounded-full bg-blue-100">
-              <BookDown size={14} />
-            </div>
-            <span className="text-xs">Title</span>
-          </div>
-          <div className="cols flex flex-col items-center">
-            <div className="flex h-10 w-10 items-center justify-center rounded-full bg-blue-100">
-              <BookDown size={14} />
-            </div>
-            <span className="text-xs">Title</span>
-          </div>
-          <div className="cols flex flex-col items-center">
-            <div className="flex h-10 w-10 items-center justify-center rounded-full bg-blue-100">
-              <BookDown size={14} />
-            </div>
-            <span className="text-xs">Title</span>
-          </div>
-          <div className="cols flex flex-col items-center">
-            <div className="flex h-10 w-10 items-center justify-center rounded-full bg-blue-100">
-              <BookDown size={14} />
-            </div>
-            <span className="text-xs">Title</span>
-          </div>
-          <div className="cols flex flex-col items-center">
-            <div className="flex h-10 w-10 items-center justify-center rounded-full bg-blue-100">
-              <BookDown size={14} />
-            </div>
-            <span className="text-xs">Title</span>
-          </div>
-        </div>
+            <span className="text-xs font-semibold">Deposit</span>
+          </li>
+          <li className="packages cols flex flex-col items-center">
+            <Link href="/dashboard/packages">
+              <div className="flex h-10 w-10 items-center justify-center rounded-full bg-blue-100">
+                <PackageOpen size={15} />
+              </div>
+            </Link>
+            <span className="text-xs font-semibold">Packages</span>
+          </li>
+        </ul>
       </section>
+      {showModal && <TopupModal closeModal={setShowModal} />}
     </>
   );
 };
